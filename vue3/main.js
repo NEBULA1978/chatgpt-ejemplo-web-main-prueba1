f8bac58474317d6118b226396385ba560fd4917c
NEBULA1978 <rpascualey@gmail.com>
2023-09-24 03:14:43 +0200

    Añado hola vue3 con cdn corregido

--- vue3/main.js
+++ vue3/main.js
@@ -1,31 +1,27 @@
-// main.js
-const bicicletasContenedor = document.getElementById("bicicletas-contenedor");
+const app = Vue.createApp({});
 
-// Obtener los datos de las bicicletas
-fetch("data.json")
-    .then(response => response.json())
-    .then(data => {
-        // Almacenar los datos en una variable global
-        window.bicicletas = data.bicicletas;
-        // Crear una instancia de Vue
-        new Vue({
-            el: "#bicicletas-contenedor", // Monta Vue en el elemento con el id "bicicletas-contenedor"
-            data: {
-                bicicletas: data.bicicletas,
-                filtroModelo: "",
-                filtroPrecio: 0
-            },
-            methods: {
-                // Función para mostrar las bicicletas en la página
-                mostrarBicicletas: function () {
-                    // Lógica para mostrar las bicicletas aquí
-                    // Puedes acceder a los datos con this.bicicletas, this.filtroModelo, this.filtroPrecio
-                }
-            },
-            mounted: function () {
-                // Llama a la función mostrarBicicletas una vez que Vue se haya montado
-                this.mostrarBicicletas();
-            }
-        });
-    })
-    .catch(error => console.error(error));
+app.component('mi-componente', {
+  template: `
+    <div>
+      <h1>Mi primer componente Vue</h1>
+      <p>{{ mensaje }}</p>
+      <button @click="cambiarMensaje">Cambiar Mensaje</button>
+    </div>
+  `,
+  data() {
+    return {
+      mensaje: '¡Hola desde Vue!'
+    };
+  },
+  methods: {
+    cambiarMensaje() {
+      if (this.mensaje === '¡Hola desde Vue!') {
+        this.mensaje = 'Mensaje cambiado';
+      } else {
+        this.mensaje = '¡Hola desde Vue!';
+      }
+    }
+  }
+});
+
+app.mount('#app');
